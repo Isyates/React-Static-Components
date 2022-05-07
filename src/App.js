13f3961e31b11463dc412f7a2d8e48700419cdf0
Isiah Yates <isyates@outlook.com>
2022-05-07 00:12:33 -0700

    adds styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-import './App.css';
+import './App.css'
 import Analysis from './components/Analysis'
 import AverageRating from './components/AverageRating'
 import Reviews from './components/Reviews'
@@ -12,11 +11,13 @@ import WebsiteVisitors from './components/WebsiteVisitors'
 function App() {
   return (
     <div className="App">
-      <Analysis />
-      <AverageRating/>
-      <Reviews/>
-      <Dashboard/>
-      <WebsiteVisitors/>
+      <>
+      <div className="Dashboard"><Dashboard/></div>
+      <div className="Analysis"><Analysis/> </div>
+     <div className="AverageRating"> <AverageRating/> </div>
+      <div className="Reviews"><Reviews/> </div>
+      <div className="WebsiteVisitors"><WebsiteVisitors/> </div>
+      </>
     </div>
   );
 }
---
+++ src/components/WebsiteVisitors.js
@@ -0,0 +1,9 @@
+function WebsiteVisitors(){
+    return(
+        <>
+        <h1>Website Visitors</h1>
+        <h2>821</h2>
+        </>
+    )
+}
+export default WebsiteVisitors
